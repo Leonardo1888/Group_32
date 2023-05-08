a0d7c3762e280af31c4ed13be9dcf49881fe0b57
Pietro <127401246+pietrobonomelli@users.noreply.github.com>
2023-05-08 14:24:31 +0200

    added personal goal card class

--- src/logic/Player.java
+++ src/logic/Player.java
@@ -4,10 +4,12 @@ public class Player {
 	private int id;
 	private String username;
 	private Bookshelf shelf;
+	private PersonalGoalCard personalGoal;
 	
-	public Player (int id, String username, Bookshelf shelf) {
+	public Player (int id, String username, Bookshelf shelf, PersonalGoalCard personalGoal) {
 		this.id = id;
 		this.username = username;
 		this.shelf = shelf;
+		this.personalGoal = personalGoal;
 	}
 }
