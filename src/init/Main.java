645413c6974be869531cbd7da2b1ccb4ec851073
Pietro <127401246+pietrobonomelli@users.noreply.github.com>
2023-05-08 14:27:45 +0200

    updated main with personal goal card

--- src/init/Main.java
+++ src/init/Main.java
@@ -5,19 +5,22 @@ import logic.*;
 public class Main {
 
 	public static void main(String[] args) {
-		final int NPLAYERS = 3;
+		final int NPLAYERS = 2;
 		Board board = new Board(NPLAYERS);
 		board.printBoard();
 		
 		
 		Bookshelf shelf1 = new Bookshelf();
-		Player Player1 	 = new Player(1, "Leonardo", shelf1);
+		PersonalGoalCard personalGoal1 = new PersonalGoalCard();
+		Player Player1 	 = new Player(1, "Leonardo", shelf1, personalGoal1);
 		
 		Bookshelf shelf2 = new Bookshelf();
-		Player Player2 	 = new Player(2, "Pietro", shelf2);
+		PersonalGoalCard personalGoal2 = new PersonalGoalCard();
+		Player Player2 	 = new Player(2, "Pietro", shelf2, personalGoal2);
 		
 		
 		shelf1.printBoard();
+		personalGoal1.printBoard();
 	}
 
 }
